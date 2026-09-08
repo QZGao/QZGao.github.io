113439ac167b935d0aa72f815f20fd87b6c63f48
Quinn Gao <gaoqz.cs@gmail.com>
2026-09-08 15:12:06 +0800

    Unify skin variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
-      theme: 'github-light',
+      themes: { light: 'github-light', dark: 'github-dark' },
       wrap: true,
     },
   },
